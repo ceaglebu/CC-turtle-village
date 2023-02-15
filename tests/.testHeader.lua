548faf01d147935c479da3b4ef2cb36390420054
ceaglebu <craig.eagleburger@gmail.com>
2023-02-15 00:54:23 -0500

    Added test files to run code in terminal

---
+++ tests/.testHeader.lua
@@ -0,0 +1,1 @@
+require("CC-turtle-village/movement")
---
+++ tests/setPos.lua
@@ -0,0 +1,17 @@
+require("CC-turtle-village/tests/.testHeader")
+
+local tArgs = {...}
+
+if #tArgs < 4 then
+    print("Usage: setPos <x y z dir>")
+    return
+end
+
+pos = {
+    x = tonumber(tArgs[1]),
+    y = tonumber(tArgs[2]),
+    z = tonumber(tArgs[3]),
+    dir = directions.tArgs[4]
+}
+
+UpdatePos(pos)
